6fd5d5778bccabb7838cfc4a6423edbf2e11415d
jssgarcia <jssgarciar@gmail.com>
2023-02-26 15:29:07 +0100

    Boletin 4.2 Fix secret constants

--- Bloque1/Tema4/Boletines/4.2/constants.py
+++ Bloque1/Tema4/Boletines/4.2/constants.py
@@ -1,11 +1,8 @@
 class Constants:
 
-    REVERSE_GEOCODE_APIKEY='6b78b9272emshed18bd54d65e09bp16f409jsn0b64b501a7f0'
-
     TELEGRAM_BOT_NAME='IESAH 23LMSGI TEACHER-TEST'
     TELEGRAM_USERNAME='iesah_23lmsgi_teacher_test_bot'
 
-    TELEGRAM_BOT_TOKEN = "6020976216:AAF0u2Dmq3Any68J3M1DI5fDm8E8sDV-9ic"
     WEATHER_API_KEY = '<your-open-weather-map-api-key>'
 
     WELCOME_MESSAGE = 'Welcome to ChivalrouS_Bot. I am here for make easier your life... :)'
